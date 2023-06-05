28a61d824c3fc3425d298e19231b16b695cfeae5
Nastura <chulmost@gmail.com>
2023-06-05 15:56:25 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        bonusService service = new bonusService();
+//        int expected = 300;
+//        int actual = (int) service.calculate(1_000, true);
+//        System.out.println("1. " + expected + " == ? == " + actual);
+//
+//
+//        expected = 500;
+//        actual = (int) service.calculate(1_000_000, true);
+//        System.out.println("1. " + expected + " == ? == " + actual);
+//
+//    }
+//
+//}
---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,38 @@
+//  import org.example.bonusService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BonusServiceTest {
+
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        bonusService service = new bonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        bonusService service = new bonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+}
